3c3a252bc103982397666158790fd10c7a70b84c
KasunHasanga <kasunhasanga96@gmail.com>
2020-12-15 00:58:57 +0530

    Add more dummy data for Chat

--- lib/Screen/WhatsAppCHATS.dart
+++ lib/Screen/WhatsAppCHATS.dart
@@ -9,31 +9,39 @@ class WhatsAppChats extends StatefulWidget {
 class _WhatsAppChatsState extends State<WhatsAppChats> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ListView.builder(
-      itemCount: dummyData.length,
-      itemBuilder: (context, index) => Column(
-        children: <Widget>[
-          Divider(
-            height: 10.0,
-          ),
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.greenAccent,
-              backgroundImage: NetworkImage(dummyData[index].avatorURL),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(dummyData[index].name),
-                Text(dummyData[index].time,style: TextStyle(fontSize: 13),),
-              ],
-            ),
-            subtitle: Text(dummyData[index].massage),
-          )
-        ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.message,color: Colors.white,),
+        onPressed: (){
+          print('Press message');
+        },
       ),
-    ));
+      body: Container(
+          child: ListView.builder(
+        itemCount: dummyData.length,
+        itemBuilder: (context, index) => Column(
+          children: <Widget>[
+            Divider(
+              height: 10.0,
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.greenAccent,
+                backgroundImage: NetworkImage(dummyData[index].avatorURL),
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(dummyData[index].name),
+                  Text(dummyData[index].time,style: TextStyle(fontSize: 13),),
+                ],
+              ),
+              subtitle: Text(dummyData[index].massage),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
