5d54c715525b3cc2943bf990dcb09a211af66b9d
KasunHasanga <kasunhasanga96@gmail.com>
2020-12-14 21:41:03 +0530

    Initialization whatsapp Clone App

---
+++ android/app/src/main/kotlin/com/kasunhasanga/whatsapp_clone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kasunhasanga.whatsapp_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screen/HomePage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+
+        leading: Row(
+          children: [
+            Icon(Icons.search,
+            color: Colors.black87,)
+          ],
+        ),
+      ),
+      body: Container(
+
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
