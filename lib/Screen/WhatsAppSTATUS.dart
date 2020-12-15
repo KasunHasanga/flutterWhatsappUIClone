22225f3b41daef12d1a35826a308169f90b0f0ea
KasunHasanga <kasunhasanga96@gmail.com>
2020-12-15 14:35:37 +0530

    Make Status Page with dummy Data

--- lib/Screen/WhatsAppSTATUS.dart
+++ lib/Screen/WhatsAppSTATUS.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_app/models/StatusModel.dart';
 
 class WhatsAppStatus extends StatefulWidget {
   @override
@@ -6,10 +8,83 @@ class WhatsAppStatus extends StatefulWidget {
 }
 
 class _WhatsAppStatusState extends State<WhatsAppStatus> {
+  ScrollController _scrollController;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('STATUS')),
+    return Scaffold(
+      body: SingleChildScrollView(
+
+        child: Column(
+          
+          children: [
+            ListTile(
+              leading: CircleAvatar(backgroundImage: null,backgroundColor: Colors.yellow,),
+              title: Text('My Status'),
+              subtitle: Text('Tap to add status update'),
+            ),
+            Container(
+             width: double.infinity,
+              decoration: BoxDecoration(
+
+                color: Colors.grey,
+              ),
+
+              padding: EdgeInsets.symmetric(horizontal:5,vertical: 8 ),
+              child:Text(
+                'Recent Update',
+
+                style: TextStyle(
+
+                    color: Colors.black54
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            ListView.builder(
+              controller: _scrollController,
+                itemCount: dummyStatusData.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) =>Column(
+                  children: [
+                    Divider(height: 10,),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.greenAccent,
+                        backgroundImage: NetworkImage(dummyStatusData[index].StatusURL),),
+                      title: Text(dummyStatusData[index].name),
+                      subtitle: Text(dummyStatusData[index].time),
+
+                    )
+                  ],
+                ),
+            ),
+
+
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: (){
+              print('press type status');
+            },
+            backgroundColor: Colors.white60,
+
+            child: Icon(Icons.phone_enabled_sharp),
+          ),
+          SizedBox(height: 10,),
+          FloatingActionButton(
+            onPressed: (){
+              print('press camera Button');
+            },
+
+            child: Icon(Icons.camera_alt_rounded,color: Colors.white,),
+          ),
+        ],
+      )
     );
   }
 }
--- lib/models/ChatModels.dart
+++ lib/models/ChatModels.dart
@@ -1,40 +1,40 @@
 import 'package:flutter/material.dart';
 
-class ChatModels{
+class ChatModel{
 
   String name;
   String massage;
   String time;
   String avatorURL;
-  ChatModels({this.name,this.massage,this.time,this.avatorURL});
+  ChatModel({this.name,this.massage,this.time,this.avatorURL});
 }
 
-List<ChatModels> dummyData=[
-  ChatModels(
+List<ChatModel> dummyData=[
+  ChatModel(
     name: 'Kasun Hasanga',
     massage: 'This Is First Message',
     time: "11:44 pm",
     avatorURL: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
   ),
-  ChatModels(
+  ChatModel(
       name: 'kelum Sadaruwan',
       massage: 'This is Second Message',
       time: "now",
       avatorURL: 'https://823802.smushcdn.com/1622828/wp-content/uploads/2017/10/Understanding-BPD-Emotional-Manipulation-Techniques-and-How-Treatment-Can-Help-768x512.jpg?lossy=1&strip=1&webp=1'
   ),
-  ChatModels(
+  ChatModel(
       name: 'namal udugama',
       massage: 'This Is third Message',
       time: "11:44 am",
       avatorURL: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
   ),
-  ChatModels(
+  ChatModel(
       name: 'Kasun Kalhara',
       massage: 'This Is fourth Message',
       time: "6:05 pm",
       avatorURL: 'https://api.time.com/wp-content/uploads/2017/12/terry-crews-person-of-year-2017-time-magazine-2.jpg?w=800&quality=85'
   ),
-  ChatModels(
+  ChatModel(
       name: 'Sumudu perera',
       massage: 'This Is fifth\' Message',
       time: "1:03 am",
